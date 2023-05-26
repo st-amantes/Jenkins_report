f1756f13e2aea570f42e1c4a44701f9c19aec1f3
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-26 22:04:04 +0700

    test passed

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+from dotenv import load_dotenv
 
 from utilits import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -13,26 +17,40 @@ def pytest_addoption(parser):
         default='100.0'
     )
 
-@pytest.fixture(scope="function")
-def driver_setting(request):
-    selenoid_browser =request.config.getoption("--browser")
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
-        "browserName": selenoid_browser,
-        "browserVersion": "100.0",
+        "browserName": "chrome",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
     browser = Browser(Config(driver=driver))
-    yield
 
-    attach.add_html(browser)
+    yield browser
+
     attach.add_html(browser)
     attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
--- tests/demoqa/test_registration_forum.py
+++ tests/demoqa/test_registration_forum.py
@@ -3,40 +3,38 @@ from pages.registration_pages import RegistrationPage
 
 
 @allure.title("Тест регистрации формы")
-def test_complete_registration_demoqa(driver_setting):
-    # открытие регистрации
+def test_successful(setup_browser):
+    browser = setup_browser
     registration_page = RegistrationPage()
-    browser = driver_setting
-    # заполнение формы
-    registration_page.fill_first_name("Albert")
-    registration_page.fill_last_name("Ivanov")
-    registration_page.fill_email("ALLIIVAN@mail.ru")
-    registration_page.fill_number("8954567689")
-    registration_page.choice_gender('Male')
-    registration_page.fill_date_of_birth('1993', 'November', '28')
-    registration_page.choice_subject('Physics')
-    registration_page.choice_hobbies('Sports')
-    registration_page.choice_hobbies_more('Reading')
-    registration_page.choice_hobbies_more_more('Music')
-    registration_page.choice_pictures('pictures.jpg')
-    registration_page.remove_block()
-    registration_page.fill_adress('Pharabi street 18')
-    registration_page.fill_state('Rajasthan')
-    registration_page.fill_city('Jaipur')
+    browser.open("https://demoqa.com/automation-practice-form")
 
-    # THEN
-    # проверка формы
-    registration_page.assert_register_user_info(
-        'Albert Ivanov',
-        'ALLIIVAN@mail.ru',
-        'Male',
-        '8954567689',
-        '28 November,1993',
-        'Physics',
-        'Sports, Reading, Music',
-        'picture',
-        'Pharabi street 18',
-        'Rajasthan Jaipur')
+    @allure.step("Fill form")
+    def fill_form():
+        registration_page.fill_first_name("Albert")
+        registration_page.fill_last_name("Ivanov")
+        registration_page.fill_email("ALLIIVAN@mail.ru")
+        registration_page.fill_number("8954567689")
+        registration_page.choice_gender('Male')
+        registration_page.fill_date_of_birth('1993', 'November', '28')
+        registration_page.choice_subject('Physics')
+        registration_page.choice_hobbies('Sports')
+        registration_page.choice_hobbies_more('Reading')
+        registration_page.choice_hobbies_more_more('Music')
+        registration_page.choice_pictures('pictures.jpg')
+        registration_page.remove_block()
+        registration_page.fill_adress('Pharabi street 18')
+        registration_page.fill_state('Rajasthan')
+        registration_page.fill_city('Jaipur')
 
-
-
+        # THEN
+        registration_page.assert_register_user_info(
+            'Albert Ivanov',
+            'ALLIIVAN@mail.ru',
+            'Male',
+            '8954567689',
+            '28 November,1993',
+            'Physics',
+            'Sports, Reading, Music',
+            'picture',
+            'Pharabi street 18',
+            'Rajasthan Jaipur')
