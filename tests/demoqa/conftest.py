55cd088d0bae433cd140d1ba1af5fe0dc521fe3e
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-26 15:01:41 +0700

    delete simple

--- tests/demoqa/conftest.py
+++ tests/demoqa/conftest.py
@@ -5,12 +5,8 @@ from selene.support import webdriver
 
 @pytest.fixture(scope="function", autouse=True)
 def driver_setting():
-    chrome_options = webdriver.CromeOptions()
-    browser.config.driver_options = chrome_options
     browser.config.base_url = "https://demoqa.com"
     browser.driver.set_window_size(1980, 1080)
-
-
     yield
 
     browser.quit()
--- tests/simple/test_skip.py
+++
@@ -1,9 +0,0 @@
-import pytest
-@pytest.mark.skip
-def test_skipp1():
-    pass
-
-@pytest.mark.skip
-def test_skipp2():
-    pass
-
