101f65a01b3276e2be41f16da3e71fd7b220f4c4
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-26 14:46:16 +0700

    add allure screenshot

--- tests/demoqa/conftest.py
+++ tests/demoqa/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selene import browser
+from selene.support import webdriver
 
 
 @pytest.fixture(scope="function", autouse=True)
 def driver_setting():
+    chrome_options = webdriver.CromeOptions()
+    browser.config.driver_options = chrome_options
     browser.config.base_url = "https://demoqa.com"
     browser.driver.set_window_size(1980, 1080)
 
